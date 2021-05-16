0307938f86bb5478032e90882745d98e4b11679b
Kim HyunWoo <stories2@naver.com>
2021-05-16 22:40:52 +0900

    Update thecamp.js

--- functions/Core/thecamp.js
+++ functions/Core/thecamp.js
@@ -23,44 +23,36 @@ exports.sendLetter = (thecampInfo, admin, res) => {
         .then(code => checkLetterList(cookie, thecampInfo, myNickname))
         .then(currentLetterList => {
             if (currentLetterList.length === letterList.length + 1) {
+                console.log('letter len ok', currentLetterList.length, ' <=> ', letterList.length + 1)
                 ref.push().set({
                     status: true,
                     message: 'OK',
                     time: time
                 }, err => {
-                    if (err) {
-                        console.error(`Cannot save data ${err.message}`)
-                        res.send({
-                            success: false,
-                            message: `Cannot save data ${err.message}`
-                        })
+                    if(err) {
+                        this.handleError(err, res);
                     } else {
                         res.send({
                             success: true,
-                            message: 'Good bye bro.'
                         })
                     }
                 })
                 return true;
             } else {
+                console.log('letter len mismatch', currentLetterList.length, ' <=> ', letterList.length + 1)
                 ref.push().set({
                     status: false,
                     message: `Letter size not match ${currentLetterList.length} <-> ${letterList.length} + 1`,
                     time: time
                 }, err => {
-                    const result = {
-                        success: false,
-                        message: `Letter size not match ${currentLetterList.length} <-> ${letterList.length} + 1`
+                    if(err) {
+                        this.handleError(err, res);
+                    } else {
+                        res.send({
+                            success: false,
+                            message: 'msg len mismatch'
+                        })
                     }
-                    if (err) {
-                        console.error(`Cannot save data ${err.message}`)
-                        result.message += `\nCannot save data, Letter  ${err.message}`
-                    } 
-                    
-                    res.send({
-                        success: false,
-                        message: `Cannot save data, Letter  ${err.message}`
-                    })
                 })
                 return false;
             }
@@ -70,38 +62,42 @@ exports.sendLetter = (thecampInfo, admin, res) => {
                 status: false,
                 message: err,
                 time: time
-            }, err => {
-                const result = {
-                    success: false,
-                    message: `${err}`
-                }
-                if (err && err.message) {
-                    console.error(`Cannot save data ${err.message}`)
-                    result.message += `\nCannot save data, Letter  ${err.message}`
-                
-                    res.send({
-                        success: false,
-                        message: `Cannot save data, Letter  ${err.message}`
-                    })
-                } else if (err) {
-                    console.error(`Cannot save data ${err}`)
-                    result.message += `\nCannot save data, Letter  ${err}`
-                
-                    res.send({
-                        success: false,
-                        message: `Cannot save data, Letter  ${err}`
-                    })
-                } else {
-                    console.error(`Cannot save data [Undefined error]`)
-                    result.message += `\nCannot save data, Letter [Undefined error]`
-                
-                    res.send({
-                        success: false,
-                        message: `Cannot save data, Letter [Undefined error]`
-                    })
-                }
+            }, err2 => {
+                this.handleError(err2 || err, res);
             })
         })
+}
+
+exports.handleError = (err, res) => {
+    const result = {
+        success: false,
+        message: `${err}`
+    }
+    if (err && err.message) {
+        console.error(`Cannot save data ${err.message}`)
+        result.message += `\nCannot save data, Letter  ${err.message}`
+    
+        res.send({
+            success: false,
+            message: `Cannot save data, Letter  ${err.message}`
+        })
+    } else if (err) {
+        console.error(`Cannot save data ${err}`)
+        result.message += `\nCannot save data, Letter  ${err}`
+    
+        res.send({
+            success: false,
+            message: `Cannot save data, Letter  ${err}`
+        })
+    } else {
+        console.error(`Cannot save data [Undefined error]`)
+        result.message += `\nCannot save data, Letter [Undefined error]`
+    
+        res.send({
+            success: false,
+            message: `Cannot save data, Letter [Undefined error]`
+        })
+    }
 }
 
 exports.getLetterList = (thecampInfo, res) => {
@@ -147,23 +143,46 @@ const crawlDaumPopularNews = () => {
         const request = require('request');
         const options = {
             uri: 'https://news.daum.net/ranking/popular',
+            // uri: 'https://google.com',
             method: 'GET',
-            headers: {
-                accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                'accept-encoding': 'gzip, deflate, br',
-                'accept-language': 'en-US,en;q=0.9,ko;q=0.8',
-                'cache-control': 'no-cache',
-                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36'
-            }
+            // headers: {
+            //     accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            //     'accept-encoding': 'gzip, deflate, br',
+            //     'accept-language': 'en-US,en;q=0.9,ko;q=0.8',
+            //     'cache-control': 'no-cache',
+            //     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36'
+            // }
         }
         request.get(options, (err,httpResponse,body) => {
+            // console.log('httpResponse', httpResponse);
             if (err) {
                 reject(new Error(`Error while get daumn popular news site ${err.message}`));
                 return;
             }
-            const $ = cheerio.load(body);
-            const newsListStr = $('.list_news2 > li').text().trim().replace(/\s/g, '');
-            // console.log('body', body);
+            const $ = cheerio.load(httpResponse.body);
+            const newsList = $('ul[class=list_news2]').children('li')//.html()//.trim().replace(/\s/g, '');
+            // console.log('body', httpResponse.body);
+            let newsListStr = '';
+            console.log('newsList len', newsList.length);
+            const newListArr = [];
+            newsList.each((i, ele) => {
+                console.log('----\n',$(ele).children('.rank_num.rank_popular').text().trim())
+                console.log('-title-\n', $($(ele).children('.cont_thumb')).children('.tit_thumb').text().trim())
+                console.log('-body-\n', $($(ele).children('.cont_thumb')).children('.desc_thumb').text().trim())
+                newListArr.push(`[${$(ele).children('.rank_num.rank_popular').text().trim()}]\n<b>title: ${$($(ele).children('.cont_thumb')).children('.tit_thumb').text().trim()}</b><br>\nbody:${$($(ele).children('.cont_thumb')).children('.desc_thumb').text().trim()}`)
+                // rank = ele.contents().children('span[class=screen_out]');
+                // console.log(rank.text())
+            })
+            newsListStr = newListArr.join('<br>');
+            // if(newsList && newsList[0]) {
+            //     newsList[0].children.forEach(child => {
+            //             console.log('test2', child.childNodes);
+            //             // childCheerio = cheerio.load(child.html());
+            //         // if (child.prev.children) {
+
+            //         // }
+            //     })
+            // }
             console.log('news list: ', newsListStr);
             resolve(newsListStr);
         })
@@ -202,7 +221,7 @@ const writeLetter = (cookie, thecampInfo, lastCount, content) => {
     return new Promise((resolve, reject) => {
         if (!content || content.length <= 0) {
             console.log('Empty content detected!');
-            reject('Empty content detected!');
+            reject(new Error('Empty content detected!'));
             return;
         }
         const request = require('request');
@@ -258,7 +277,8 @@ const checkLetterList = (cookie, thecampInfo, myNickname) => {
             console.log('Check letter list body ', body);
             const result = JSON.parse(body);
             if (result.resultCd === '0000') {
-                resolve(result.listResult.filter(i => i.nickname === myNickname));
+                console.log('Check letter list api response: ', result.resultCd, result.listResult.filter(i => i.send_name === thecampInfo.send_name).length);
+                resolve(result.listResult.filter(i => i.send_name === thecampInfo.send_name));
             } else {
                 reject(new Error(`Error while check letter list ${result.resultCd}`))
             }
